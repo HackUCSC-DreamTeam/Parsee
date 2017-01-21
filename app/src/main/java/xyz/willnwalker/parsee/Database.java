984856d579e5b752b3359c10718001af97dd2d2e
imWillX <wxu13@student.mtsac.edu>
2017-01-21 10:14:35 -0800

    Update

--- app/src/main/java/xyz/willnwalker/parsee/Database.java
+++ app/src/main/java/xyz/willnwalker/parsee/Database.java
@@ -6,22 +6,35 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+
+ Database database = new Database();
+ database.createUser("0", "Will");
+ database.createUser("1", "Kreg");
+
  * Created by willi on 1/21/2017.
  */
 
 public class Database {
 
+    Database instance;
     FirebaseDatabase database;
     DatabaseReference mDatabase;
 
     final String KEY = "USERS";
 
+    volatile List<User> users;
+
     public Database() {
         database = FirebaseDatabase.getInstance();
         mDatabase = database.getReference(KEY);
+        users = new ArrayList<>();
         addListener();
     }
 
@@ -31,20 +44,40 @@ public class Database {
         return user;
     }
 
-    public void getUser(String uid) {
-    }
-
     public void deleteUser(String uid) {
         mDatabase.child(uid).removeValue();
     }
 
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
 
-    public void addListener() {
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public User getUser(String uid) {
+        Log.d("getUser", uid + " " + getUsers().size());
+
+        int i = 0;
+        for(User user: getUsers()) {
+            if(user.uid.equals(uid)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    private void addListener() {
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String value = dataSnapshot.getValue(String.class);
-                Log.d("value", value);
+            public void onDataChange(final DataSnapshot dataSnapshot) {
+                GenericTypeIndicator<List<User>> t = new GenericTypeIndicator<List<User>>() {};
+
+                List<User> users = dataSnapshot.getValue(t);
+                setUsers(users);
+                Log.d("Listener", users.toString() + " " + getUsers().size());
+                Log.d("Database Test", getUser("0") + "");
             }
 
             @Override
@@ -56,5 +89,9 @@ public class Database {
 
     public void removeListeners() {
 
+    }
+
+    public Database getInstance() {
+        return instance;
     }
 }
--- app/src/main/java/xyz/willnwalker/parsee/User.java
+++ app/src/main/java/xyz/willnwalker/parsee/User.java
@@ -1,6 +1,9 @@
 package xyz.willnwalker.parsee;
 
+import android.location.Location;
+
 import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,11 +14,14 @@ import java.util.Map;
  * Created by willi on 1/21/2017.
  */
 
+@IgnoreExtraProperties
 public class User {
 
-    String uid;
-    String displayName;
-    List<User> group;
+    public String uid;
+    public String displayName;
+    public String testString;
+    public List<User> friends;
+    public Location myLocation;
 
     public User() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
@@ -24,16 +30,24 @@ public class User {
     public User(String uid, String displayName) {
         this.uid = uid;
         this.displayName = displayName;
-        this.group = new ArrayList<>();
+        this.testString = "99";
+        this.friends = new ArrayList<>();
     }
 
     @Exclude
     public Map<String, Object> toMap() {
         HashMap<String, Object> result = new HashMap<>();
         result.put("uid", uid);
+        result.put("testString", testString);
         result.put("displayName", displayName);
-        result.put("group", group);
+        result.put("friends", friends);
+        result.put("location", myLocation);
 
         return result;
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + this.uid + " name: " + this.displayName;
     }
 }
